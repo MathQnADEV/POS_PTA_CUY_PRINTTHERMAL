804e21e064725a10c7bf75d7c5635c74a39a4b90
MathQnADEV <mathqna.project@gmail.com>
2024-03-10 17:49:06 +0800

    QRIS NOT USING MIDTRANS FIXX

---
+++ lib/presentation/home/bloc/checkout/checkout_event.dart
@@ -0,0 +1,10 @@
+part of 'checkout_bloc.dart';
+
+@freezed
+class CheckoutEvent with _$CheckoutEvent {
+  const factory CheckoutEvent.started() = _Started;
+  // Add Checkout
+  const factory CheckoutEvent.addCheckout(Product product) = _AddCheckout;
+  // Remove Checkout
+  const factory CheckoutEvent.removeCheckout(Product product) = _RemoveCheckout;
+}
---
+++ lib/presentation/home/bloc/product/product_bloc.dart
@@ -0,0 +1,74 @@
+// ignore_for_file: public_member_api_docs, sort_constructors_first
+import 'package:bloc/bloc.dart';
+import 'package:flutter_pos_app/data/datasources/product_local_datasource.dart';
+import 'package:flutter_pos_app/data/models/request/product_request_model.dart';
+import 'package:freezed_annotation/freezed_annotation.dart';
+
+import 'package:flutter_pos_app/data/datasources/product_remote_datasource.dart';
+import 'package:flutter_pos_app/data/models/response/product_response_model.dart';
+import 'package:image_picker/image_picker.dart';
+
+part 'product_bloc.freezed.dart';
+part 'product_event.dart';
+part 'product_state.dart';
+
+class ProductBloc extends Bloc<ProductEvent, ProductState> {
+  final ProductRemoteDatasource _productRemoteDatasource;
+  List<Product> products = [];
+  ProductBloc(
+    this._productRemoteDatasource,
+  ) : super(const _Initial()) {
+    on<_Fetch>((event, emit) async {
+      emit(const ProductState.loading());
+      final response = await _productRemoteDatasource.getProducts();
+      response.fold(
+        (l) => emit(ProductState.error(l)),
+        (r) {
+          products = r.data;
+          emit(ProductState.success(r.data));
+        },
+      );
+    });
+
+    on<_FetchLocal>((event, emit) async {
+      emit(const ProductState.loading());
+      final localPproduct =
+          await ProductLocalDatasource.instance.getAllProduct();
+      products = localPproduct;
+      emit(ProductState.success(products));
+    });
+
+    on<_FetchByCategory>((event, emit) async {
+      emit(const ProductState.loading());
+      final newProducts = event.category == 'all'
+          ? products
+          : products
+              .where((element) => element.category == event.category)
+              .toList();
+      emit(ProductState.success(newProducts));
+    });
+
+    on<_AddProduct>((event, emit) async {
+      emit(const ProductState.loading());
+      final requestData = ProductRequestModel(
+        name: event.product.name,
+        price: event.product.price,
+        stock: event.product.stock,
+        category: event.product.category,
+        isBestSeller: event.product.isBestSeller ? 1 : 0,
+        image: event.image,
+      );
+      final response = await _productRemoteDatasource.addProduct(requestData);
+
+      response.fold(
+        (l) => emit(ProductState.error(l)),
+        (r) {
+          products.add(r.data);
+          emit(ProductState.success(products));
+        },
+      );
+
+      emit(ProductState.success(products));
+    });
+  }
+}
